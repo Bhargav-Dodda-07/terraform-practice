8691bd7d77e14227fca892f09cc689f987f9e0da
Bhargav-Dodda-07 <bhargavdodda969@gmail.com>
2026-02-04 13:51:32 +0530

    dynamic sg

---
+++ loops/count/variables.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+    default = ["mongodb", "mysql", "redis", "rabbitmq"]
+}
+
+variable "zone_id" {
+    default = "Z01258982K7OWMGLN4CZ7"
+}
+
+variable "domain_name" {
+    default = "daws-86s.space"
+}
