36d2b6b9cfbe6063558f99979294eebea974491a
Bhargav-Dodda-07 <bhargavdodda969@gmail.com>
2026-02-05 11:15:17 +0530

    provisioners remote-exec

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -10,10 +10,33 @@ resource "aws_instance" "terraform" {
 
     provisioner "local-exec" {
         command = "echo ${self.private_ip} > inventory"
+        on_failure = continue
     }
 
     provisioner "local-exec" {
         command = "echo instance is destroyed"
+        when = destroy
+    }
+
+    connection {
+        type     = "ssh"
+        user     = "ec2-user"
+        password = "DevOps321"
+        host     = self.public_ip
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo dnf install nginx -y",
+            "sudo systemctl start nginx"
+        ]
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo systemctl stop nginx",
+            "echo 'successfully stopped nginx'"
+        ]
         when = destroy
     }
 }
